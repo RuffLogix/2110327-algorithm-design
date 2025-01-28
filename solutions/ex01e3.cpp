935de19cba9090f6003efbca466fb4193fc1871d
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2025-01-28 20:57:31 +0700

    add: ex01e3.cpp

---
+++ solutions/ex01e3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[100001];
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int n, t;
+    cin >> n >> t;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }    
+
+    sort(arr+1, arr+1+n);
+
+    while (t--) {
+        int x;
+        cin >> x;
+
+        int ans = upper_bound(arr+1, arr+1+n, x) - arr - 1;
+        cout << (ans ? arr[ans] : -1) << '\n';
+    }
+
+    return 0;
+}
